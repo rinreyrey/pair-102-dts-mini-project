521d70b6bcd4b5f8ea76dd5598d636d06cd90778
Rindang Reynaldi <rindangr79@gmail.com>
2022-07-21 16:56:06 +0700

    Rindang Reynaldi

--- src/containers/Copyright.jsx
+++ src/containers/Copyright.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Typography,Link } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 export default function Copyright(props) {
     return (
       <Typography variant="body2" color="text.secondary" align="center" {...props}>
         {'Copyright © '}
-        <Link color="inherit" href="/home">
+        <Link color="inherit" to={"/home"}>
           Movies
         </Link>{' '}
         {new Date().getFullYear()}
